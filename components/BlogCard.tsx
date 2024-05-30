59c31aa500ce7118e24ff6f29da1cc5809a35b37
can-ok <canokan.taskiran@ogr.iu.edu.tr>
2024-05-30 03:04:07 +0300

    adds padding for bottom

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -19,7 +19,7 @@ const BlogCard:React.FC<BlogCardProps> = ({ imgUrl, title, description, contentU
             <div className="bg-[#181818]py-6 px-2">
                 <h5 className="text-left text-lg font-medium">{title}</h5>
                 <p className="py-2">{description}</p>
-                <a className="text-left text-lg font-bold" href={contentUrl} target="_blank" rel="noopener noreferrer"> Read more... </a> 
+                <a className="text-left text-lg font-bold pb-4" href={contentUrl} target="_blank" rel="noopener noreferrer"> Read more... </a> 
             </div>
         </div>
     )
